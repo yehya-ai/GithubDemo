e816ae15111fe12958f2c394e908cc8a5b5543fd
jit <jit@DESKTOP-4U1LEFA>
2025-04-02 17:29:19 +0300

    Add project files.

---
+++ GithubDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GithubDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
